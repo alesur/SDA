9dee0d64337e2ea346a9e177c05215b8e3ff94e5
Alex <aleksandr.surkov@outlook.com>
2019-05-11 10:31:06 +0300

    Sum of array

--- 3_CFUN/Practice/src/SwapAPP.java
+++ 3_CFUN/Practice/src/SwapAPP.java
@@ -3,6 +3,7 @@ public class SwapAPP {
     public static void main(String[] args) {
 
        int[] digiline = new int[4];
+        int sum = 0;
         digiline[0] = 10;
         digiline[1] = 5;
         digiline[2] = 30;
@@ -10,8 +11,17 @@ public class SwapAPP {
         digiline[3]=digiline[0];
         digiline[0]=digiline[1];
         digiline[1]=digiline[3];
+        digiline[3]=0;
 
         System.out.println(digiline[0] + " " + digiline[1]+ " " + digiline[2]);
+
+        for(int i=0; i<digiline.length; i++){
+
+            sum = sum + digiline[i];
+
+        }
+        System.out.println(sum);
+
     }//psvm
 
 }//class
